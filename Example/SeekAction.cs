f92f33c73e0f00dd52994313bfa26e6e93da5348
HalfLobsterMan <766439226@qq.com>
2023-01-30 19:23:42 +0800

    增加两个行为

---
+++ Example/SeekAction.cs
@@ -0,0 +1,89 @@
+#region 注 释
+
+/***
+ *
+ *  Title:
+ *  
+ *  Description:
+ *  
+ *  Date:
+ *  Version:
+ *  Writer: 半只龙虾人
+ *  Github: https://github.com/HalfLobsterMan
+ *  Blog: https://www.crosshair.top/
+ *
+ */
+
+#endregion
+
+using CZToolKit.Common.ViewModel;
+using CZToolKit.GraphProcessor;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CZToolKit.BehaviorTree
+{
+    [NodeTooltip("追逐敌人到一定距离后停下")]
+    [NodeMenu("Seek")]
+    public class SeekAction : ActionTask
+    {
+        public float stopDistance = 2;
+        [Header("超时")] [Tooltip("超时将不再追击敌人")] public float timeout = 10;
+    }
+
+    [ViewModel(typeof(SeekAction))]
+    public class SeekActionVM : ActionTaskVM, IUpdateTask
+    {
+        public SeekActionVM(SeekAction model) : base(model)
+        {
+            Title = "追逐";
+        }
+
+        [NonSerialized] GameObject target;
+        [NonSerialized] NavMeshAgent navMeshAgent;
+
+        private float startTime;
+
+        protected override void DoStart()
+        {
+            base.DoStart();
+            var t_model = Model as SeekAction;
+            Owner.Blackboard.TryGet("Target", out target);
+            startTime = Time.time;
+            navMeshAgent.stoppingDistance = t_model.stopDistance;
+            navMeshAgent.updateRotation = true;
+            navMeshAgent.isStopped = false;
+            Debug.Log("追逐");
+        }
+
+        public void Update()
+        {
+            var t_model = Model as SeekAction;
+            var navMeshAgent = Owner.Blackboard.Get<NavMeshAgent>("NavMeshAgent");
+
+            if (target == null || !target.activeSelf || Time.time - startTime > t_model.timeout)
+            {
+                Owner.Blackboard.Set<GameObject>("Target", null);
+                navMeshAgent.isStopped = true;
+                Debug.Log("追不上");
+                Stopped(false);
+            }
+
+            if (Vector3.Distance(navMeshAgent.transform.position, target.transform.position) <= t_model.stopDistance)
+            {
+                Owner.Blackboard.Set<GameObject>("Target", null);
+                navMeshAgent.isStopped = true;
+                Stopped(true);
+            }
+
+            navMeshAgent.destination = target.transform.position;
+        }
+
+        protected override void DoStop()
+        {
+            Owner.Blackboard.Set<GameObject>("Target", null);
+            navMeshAgent.isStopped = true;
+        }
+    }
+}
---
+++ Example/WanderAction.cs
@@ -0,0 +1,96 @@
+#region 注 释
+
+/***
+ *
+ *  Title:
+ *  
+ *  Description:
+ *  
+ *  Date:
+ *  Version:
+ *  Writer: 半只龙虾人
+ *  Github: https://github.com/HalfLobsterMan
+ *  Blog: https://www.crosshair.top/
+ *
+ */
+
+#endregion
+
+using CZToolKit.Common.ViewModel;
+using CZToolKit.ECS;
+using CZToolKit.GraphProcessor;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+namespace CZToolKit.BehaviorTree
+{
+    [NodeMenu("Wander")]
+    [NodeTooltip("在指定区域内徘徊，直到看到敌人")]
+    public class WanderAction : ActionTask
+    {
+        [Header("巡逻范围")] public string center;
+        public float range = 10;
+        [Header("视野范围")] public float radius = 5;
+        [Range(0, 360)] [Header("视野角度")] public float sector = 90;
+        public LayerMask layer;
+    }
+
+    [ViewModel(typeof(WanderAction))]
+    public class WanderActionVM : ActionTaskVM, IUpdateTask
+    {
+        public WanderActionVM(WanderAction model) : base(model)
+        {
+            Title = "徘徊";
+        }
+
+        Vector3 targetPos;
+        float stayTime;
+
+        protected override void DoStart()
+        {
+            var navMeshAgent = Owner.Blackboard.Get<NavMeshAgent>("NavMeshAgent");
+            var t_model = Model as WanderAction;
+
+            targetPos = Random.insideUnitSphere * t_model.range + Owner.Blackboard.Get<GameObject>(t_model.center).transform.position;
+            targetPos.y = 0;
+            stayTime = Random.Range(2, 5);
+            navMeshAgent.stoppingDistance = 0;
+            navMeshAgent.isStopped = false;
+            Debug.Log("徘徊");
+        }
+
+        public void Update()
+        {
+            var navMeshAgent = Owner.Blackboard.Get<NavMeshAgent>("NavMeshAgent");
+            var world = Owner.Blackboard.Get<World>("World");
+            var entity = Owner.Blackboard.Get<Entity>("Entity");
+            var t_model = Model as WanderAction;
+            if (Vector3.Distance(targetPos, navMeshAgent.transform.position) <= 2)
+            {
+                stayTime -= Time.deltaTime;
+                if (stayTime <= 0)
+                {
+                    targetPos = Random.insideUnitSphere * t_model.range + Owner.Blackboard.Get<GameObject>(t_model.center).transform.position;
+                    targetPos.y = 0;
+                    stayTime = Random.Range(2, 5);
+                }
+            }
+            
+            navMeshAgent.SetDestination(targetPos);
+
+            Collider[] colliders = Physics.OverlapSphere(navMeshAgent.transform.position, t_model.radius, t_model.layer);
+            if (colliders.Length > 0)
+            {
+                foreach (var item in colliders)
+                {
+                    if (Vector3.Angle(navMeshAgent.transform.forward, item.transform.position - navMeshAgent.transform.position) <= t_model.sector / 2)
+                    {
+                        Owner.Blackboard.Set("Target", item.gameObject);
+                        Stopped(true);
+                    }
+                }
+            }
+        }
+    }
+}
